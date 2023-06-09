049587edb60dc29b447c93f4b1ca649faa379620
Zishan <szishan2902@gmail.com>
2023-06-09 18:27:40 +0530

    Add basic style

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 const result = document.querySelector(".result");
 const result1 = document.querySelector(".result1");
+const computer = document.querySelector(".computer")
 
 function getComputerChoice() {
     let random = Math.floor(Math.random()*3);
@@ -21,38 +22,47 @@ function round(playerSelection, computerSelection) {
     computerSelection = computerSelection.toLowerCase();
     if(playerSelection == "rock" && computerSelection == "paper") {
         computerScore++;
+        computer.textContent = `Computer choose: ${computerSelection}`;
         result.textContent = `You loose! Paper beats Rock`;
         result1.textContent = `Player Score: ${playerScore} ComputerScore: ${computerScore}`;
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         playerScore++;
+        computer.textContent = `Computer choose: ${computerSelection}`;
         result.textContent = `You Win! Rock beats scissors`;
         result1.textContent = `Player Score: ${playerScore} ComputerScore: ${computerScore}`;
     } else if(playerSelection == "paper" && computerSelection == "scissors") {
         computerScore++;
+        computer.textContent = `Computer choose: ${computerSelection}`;
         result.textContent = `You loose! Scissors beats Paper`;
         result1.textContent = `Player Score: ${playerScore} ComputerScore: ${computerScore}`;
     } else if(playerSelection == "paper" && computerSelection == "rock") {
         playerScore++;
+        computer.textContent = `Computer choose: ${computerSelection}`;
         result.textContent = `You Win! Paper beats Rock`;
         result1.textContent = `Player Score: ${playerScore} ComputerScore: ${computerScore}`;
     } else if(playerSelection == "scissors" && computerSelection == "rock") {
         computerScore++;
+        computer.textContent = `Computer choose: ${computerSelection}`;
         result.textContent = `You loose! Rock beats Scissors`;
         result1.textContent = `Player Score: ${playerScore} ComputerScore: ${computerScore}`;
     } else if(playerSelection === computerSelection) {
+        computer.textContent = `Computer choose: ${computerSelection}`;
         result.textContent = `It's a tie!`;
         result1.textContent = `Player Score: ${playerScore} ComputerScore: ${computerScore}`;
     } else {
         playerScore++;
+        computer.textContent = `Computer choose: ${computerSelection}`;
         result.textContent = `You Win! Scissors beats Paper`;
         result1.textContent = `Player Score: ${playerScore} ComputerScore: ${computerScore}`;
     }
     if(playerScore === 5 || computerScore === 5) {
         if(playerScore > computerScore) {
+            computer.textContent = "";
             result.textContent = "";
             result1.textContent = "Player Wins";
         }
         else {
+            computer.textContent = "";
             result.textContent = "";
             result1.textContent = "Computer Wins";
         }
@@ -61,47 +71,44 @@ function round(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let result;
-    while(playerScore < 5 && computerScore < 5) {
-        let userChoice = prompt("Rock Paper or Scissors?");
-        userChoice = userChoice.toLowerCase();
-        userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1);
-        let computerChoice = getComputerChoice();
-        console.log(`computer selection: ${computerChoice}`);
-        result = round(userChoice, computerChoice);
-        if(result == 1) {
-            console.log(`You Win! ${userChoice} beats ${computerChoice}`);
-            playerScore++;
-        } else if(result == -1) {
-            console.log(`You loose ${computerChoice} beats ${userChoice}`);
-            computerScore++;
-        } else {
-            console.log("It's a tie");
-        }
-    }
-    if(playerScore > computerScore) {
-        console.log(`You Win!\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`);
-    } else {
-        console.log(`You Loose!\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`);
-    }
-}
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     let result;
+//     while(playerScore < 5 && computerScore < 5) {
+//         let userChoice = prompt("Rock Paper or Scissors?");
+//         userChoice = userChoice.toLowerCase();
+//         userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1);
+//         let computerChoice = getComputerChoice();
+//         console.log(`computer selection: ${computerChoice}`);
+//         result = round(userChoice, computerChoice);
+//         if(result == 1) {
+//             console.log(`You Win! ${userChoice} beats ${computerChoice}`);
+//             playerScore++;
+//         } else if(result == -1) {
+//             console.log(`You loose ${computerChoice} beats ${userChoice}`);
+//             computerScore++;
+//         } else {
+//             console.log("It's a tie");
+//         }
+//     }
+//     if(playerScore > computerScore) {
+//         console.log(`You Win!\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`);
+//     } else {
+//         console.log(`You Loose!\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`);
+//     }
+// }
 
 
 rock.addEventListener('click', function(e) {
     const computerChoice = getComputerChoice();
-    console.log(`Computer choose: ${computerChoice}`);
     round("rock", computerChoice);
 });
 paper.addEventListener('click', function(e) {
     const computerChoice = getComputerChoice();
-    console.log(`Computer choose: ${computerChoice}`);
     round("paper", computerChoice);
 });
 scissors.addEventListener('click', function(e) {
     const computerChoice = getComputerChoice();
-    console.log(`Computer choose: ${computerChoice}`);
     round("scissors", computerChoice);
 });
